23c67ce9e1dc12b77fc3d94a4e1f42b817ceb1ce
Arya Laddha <z5340668@unsw.edu.au>
2022-11-19 10:02:35 +1100

    final try

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,6 @@ class Navbar extends React.Component {
     render() {
         return (
             <nav className="NavbarItems">
-                <ul classname="nav-menu-left">
-                    <li><a href="#">Women</a></li>
-                    <li><a href="#">Men</a></li>
-                    <li><a href="#">Kids</a></li>
-                </ul>
-                <h3 classname="navbar-logo">Ecommerce</h3>
-                <ul classname="nav-menu-right">
-                    <li><a href="#"><i class="fa fa-search"></i></a></li>
-                    <li><a href="#"><i class="fa fa-user"></i></a></li>
-                    <li><a href="#"><i class="fa-solid fa-heart"></i></a></li>
-                    <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
-                </ul>
             </nav>
         )
     }
