a815c140d880b9ecb1674764058810e97a6f5381
Arya Laddha <z5340668@unsw.edu.au>
2022-11-19 21:39:40 +1100

    completed navbar design initial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./Navbar.css"
+import {MenuItems, MenuItems2} from './MenuItems'
 
 class Navbar extends React.Component {
     render() {
         return (
             <nav className="NavbarItems">
+                <ul classname="nav-menu-left">
+                    {MenuItems2.map((item, index) =>{
+                        return (
+                            <li key={index}><a href={item.url} classname={item.cName}>{item.title}</a></li>
+                        )
+                    })}
+                </ul>
+                Ecommerce Website
+                <ol classname="nav-menu-right">
+                    {MenuItems.map((item, index) =>{
+                        return (
+                            <li key={index}><a href={item.url} classname={item.cName}><i class={item.icon}></i></a></li>
+                        )
+                    })}
+                </ol>
             </nav>
         )
     }
