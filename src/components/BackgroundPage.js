551f409371ba48026257b12421128c8a22d746d4
Arya Laddha <z5340668@unsw.edu.au>
2022-12-06 18:18:17 +1100

    added background image

--- src/components/BackgroundPage.js
+++ src/components/BackgroundPage.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import "./BackgroundPage.css"
+import logo from '../images/frontPage.jpg'
 
 class BackgroundPage extends React.Component {
     render () {
         return (
-            <div>
-            <img src="../images/frontPage.jpg"></img>
-            </div>
+            <img src={logo} alt="logo" className="background" />
         )
     }
 }
