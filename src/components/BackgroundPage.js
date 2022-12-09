fe547e8d949ff1b3d03aa9b2cdfa3a73ccb13435
Arya Laddha <z5340668@unsw.edu.au>
2022-12-10 08:35:08 +1100

    background image and navbar done, yet to make responsive

--- src/components/BackgroundPage.js
+++ src/components/BackgroundPage.js
@@ -5,7 +5,13 @@ import logo from '../images/frontPage.jpg'
 class BackgroundPage extends React.Component {
     render () {
         return (
-            <img src={logo} alt="logo" className="background" />
+            <div className="backgroundPage">
+                <img src={logo} alt="logo" className="background" />
+                <div className="text_in_image">
+                    <h1>Background Page, Background Page</h1>
+                    <h2>Background Page, Background Page2</h2>
+                </div>
+            </div>
         )
     }
 }
