450d2eb65a565cae3efa17b866ecdda67b740748
Arya Laddha <z5340668@unsw.edu.au>
2022-12-13 09:57:37 +1100

    bestsellers part in design process

--- src/components/BestSellers/Bestsellersgrid.js
+++ src/components/BestSellers/Bestsellersgrid.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Bestsellersgrid.css'
+import image1 from '../../images/ezgif.com-gif-maker.jpg'
 
 class Bestsellersgrid extends React.Component {
     render() {
         return (
             <div className="Gridmain">
+                <div className="designmain">
+                    <img src={image1} alt="productImage" className="productImage" />
+                </div>
+                <div className="designmain"></div>
+                <div className="designmain"></div>
                 <div className="designmain"></div>
             </div>
         )
