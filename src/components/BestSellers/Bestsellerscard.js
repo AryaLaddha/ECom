144af7ffac3150cbe79e5857af0091eb911ffcee
Arya Laddha <z5340668@unsw.edu.au>
2022-12-27 03:32:39 +1100

    changes in bestsellers grid and card

---
+++ src/components/BestSellers/Bestsellerscard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Bestsellerscard.css'
+import image1 from '../../images/ezgif.com-gif-maker.jpg'
+
+class Bestsellerscard extends React.Component {
+    render() {
+        return (
+            <div classname="bestsellerscard-container">
+                Hewoo
+            </div>
+        )
+    }
+}
+
+export default Bestsellerscard;
--- src/components/BestSellers/Bestsellersgrid.js
+++ src/components/BestSellers/Bestsellersgrid.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Bestsellersgrid.css'
 import image1 from '../../images/ezgif.com-gif-maker.jpg'
+import Bestsellerscard from './Bestsellerscard'
 
 class Bestsellersgrid extends React.Component {
     render() {
         return (
             <div className="Bestsellers-grid">
-                <div className="Bestsellers-card">
+                <Bestsellerscard />
+                {/* <div className="Bestsellers-card">
                     <div className="Bestsellers-card-imagebox">
                         <img src={image1} alt="productImage" />
                     </div>
@@ -28,7 +30,7 @@ class Bestsellersgrid extends React.Component {
                         </div>
                         <a href="#">Buy now</a>
                     </div>
-                </div>
+                </div> */}
             </div>
         )
     }
