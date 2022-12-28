ddb1c0ee994c1b49c52a47a9bfbcf8f50b9a5069
Arya Laddha <z5340668@unsw.edu.au>
2022-12-28 13:58:44 +1100

    removing bestsellers card

--- src/components/BestSellers/Bestsellersgrid.js
+++ src/components/BestSellers/Bestsellersgrid.js
@@ -7,30 +7,7 @@ class Bestsellersgrid extends React.Component {
     render() {
         return (
             <div className="Bestsellers-grid">
-                <Bestsellerscard />
-                {/* <div className="Bestsellers-card">
-                    <div className="Bestsellers-card-imagebox">
-                        <img src={image1} alt="productImage" />
-                    </div>
-                    <div className="Bestsellers-card-contentbox">
-                        <h2>Nike Hoodie</h2>
-                        <div className="Bestsellers-card-size">
-                            <h3>Size:</h3>
-                            <span>S</span>
-                            <span>M</span>
-                            <span>L</span>
-                            <span>XL</span>
-                            <span>XXL</span>
-                        </div>
-                        <div className="Bestsellers-card-color">
-                            <h3>Color:</h3>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                        <a href="#">Buy now</a>
-                    </div>
-                </div> */}
+                
             </div>
         )
     }
